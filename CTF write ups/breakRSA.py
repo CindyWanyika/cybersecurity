3e16a1b62a71c8f019f82d88af90b36e90dc295e
cindy-kilonzo <cindy.kilonzo@ashesi.edu.gh>
2026-02-14 09:46:36 +0000

    broken RSA

---
+++ CTF write ups/breakRSA.py
@@ -0,0 +1,18 @@
+from Crypto.Util.number import inverse, long_to_bytes
+
+N = "your N goes here"
+e = 65537
+c = "ciphertext goes here"
+
+# since p = 2
+p = 2
+q = N // 2
+
+phi = (p-1)*(q-1) 
+
+d = inverse(e, phi)
+
+m = pow(c, d, N)
+
+print(long_to_bytes(m))
+
---
+++ CTF write ups/computeGCDPairs.py
@@ -0,0 +1,19 @@
+#pip install pycryptodome
+
+from math import gcd
+from Crypto.Util.number import inverse, long_to_bytes
+
+e = 65537
+
+Ns = [ ]#insert N values collected
+
+for i in range(len(Ns)):
+    for j in range(i+1, len(Ns)):
+        g = gcd(Ns[i], Ns[j])
+        if g != 1:
+            print("Shared prime found!")
+            print("p =", g)
+            print("Between N", i, "and N", j)
+            exit()
+
+print("No shared primes found.")
