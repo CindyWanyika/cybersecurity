5b2685676c98683f46f95b06589a8d763a33c732
cindy-kilonzo <cindy.kilonzo@ashesi.edu.gh>
2026-02-15 21:30:25 +0000

    edit RSA

--- CTF write ups/breakRSA.py
+++ CTF write ups/breakRSA.py
@@ -1,4 +1,4 @@
-from Crypto.Util.number import inverse, long_to_bytes
+#from Crypto.Util.number import inverse, long_to_bytes
 
 N = "your N goes here"
 e = 65537
@@ -9,7 +9,6 @@ p = 2
 q = N // 2
 
 phi = (p-1)*(q-1) 
-
 d = inverse(e, phi)
 
 m = pow(c, d, N)
